37a5beb2395799f5e9ee6ce0313233c56a0ab31e
Cresythe <106111373+Cresythe@users.noreply.github.com>
2022-11-15 10:04:41 -0500

    added more CSS and Javascript page added

---
+++ character.js
@@ -0,0 +1,20 @@
+// parameters for view
+const viewHeight = 800; 
+const viewLength = 600; 
+
+// variables
+let bg; 
+let character; 
+let up; 
+let down; 
+let left;
+let right; 
+let gameLoopFn; 
+
+// Aliases
+const { Application } = PIXI; 
+const loader = PIXI.Loader.Shared; 
+const { resources} = PIXI.Loader.Shared; 
+const { Sprite } = PIXI
+const { Text } = PIXI 
+
